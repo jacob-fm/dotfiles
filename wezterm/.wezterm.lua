e8dc2b8e1e0275723875c29a7fa186e04aaea109
JacobJFM <jacobfeitmann@gmail.com>
2025-08-11 16:31:53 -0400

    Basic pane navigation keybinds in wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -9,8 +9,47 @@ config.font_size = 14.0
 config.line_height = 1.0
 
 config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
 
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 28
+
+config.keys = {
+	{
+		key = "\\",
+		mods = "CMD",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "\\",
+		mods = "SHIFT|CMD",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "w",
+		mods = "CMD",
+		action = wezterm.action.CloseCurrentPane({ confirm = false }),
+	},
+	{
+		key = "k",
+		mods = "CMD",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "j",
+		mods = "CMD",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		key = "h",
+		mods = "CMD",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "l",
+		mods = "CMD",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+}
 
 return config
