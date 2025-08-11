e5483f438a9ba4dc1dd5fba47885abadf906d8a7
JacobJFM <jacobfeitmann@gmail.com>
2025-08-11 15:17:59 -0400

    decrease wezterm opacity

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -10,7 +10,7 @@ config.line_height = 1.0
 
 config.window_decorations = "RESIZE"
 
-config.window_background_opacity = 0.5
+config.window_background_opacity = 0.8
 config.macos_window_background_blur = 28
 
 return config
