26cdff00c47924fc2f8512bdbe1c2484fdef0c38
JacobJFM <jacobfeitmann@gmail.com>
2025-10-30 14:30:39 -0400

    wezterm: use default font config (hopefully get rid of wezterm glyph warning)

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,9 +2,6 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 local act = wezterm.action
 
-config.font = wezterm.font({
-	family = "JetBrains Mono NL",
-})
 config.font_size = 14.0
 config.line_height = 1.0
 
