8a5388fc796c2feeb34568939a9780272980da74
JacobJFM <jacobfeitmann@gmail.com>
2025-10-30 18:43:41 -0400

    solve missing glyphs issue

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,6 +2,9 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 local act = wezterm.action
 
+config.font = wezterm.font({
+	family = "JetBrainsMono Nerd Font",
+})
 config.font_size = 14.0
 config.line_height = 1.0
 
